faa5d352bf09568725c3c748c90ac32876fb3a7e
DavidSFuller <123096651+DavidSFuller@users.noreply.github.com>
2023-02-27 13:45:53 +0000

    Exercise 3.9

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
   skip: function (req, res) { return req.method !== 'POST' }
